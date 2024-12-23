b6b0059cf0e23794edc2d7c4d17e2ff3440a5fca
JrGustavo <gus@soypetal.com>
2024-12-23 17:22:15 -0500

    Update ShadcnForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,134 @@
-import Image from "next/image";
 
-export default function Home() {
+    "use client";
+
+import {Card, CardContent} from "@/components/ui/card";
+import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import { useForm, FormProvider } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
+import  {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
+const userSchema = z.object({
+  name: z.string({
+    required_error: "Name is required",
+  }),
+  lastName: z.string({
+    required_error: "Last name is required",
+  }),
+  age: z.string({
+    required_error: "Age is required",
+  }).transform( (val) => parseInt(val))
+});
+
+type userType = {
+  name: string,
+  lastName: string,
+  age: number,
+  status: string;
+  email: string;
+}
+
+function Page() {
+  const form = useForm <userType>({
+    resolver: zodResolver(userSchema),
+    defaultValues: {
+      name: "",
+      lastName: "",
+      age: 0,
+      email: "",
+    }
+  });
+
+  console.log(form.formState.errors)
+
+  const onSubmit = form.handleSubmit((values)=> {
+    console.log(values);
+  })
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <Card>
+        <CardContent>
+          <FormProvider {...form}>
+            <form className="flex flex-col gap-y-2" onSubmit={onSubmit}>
+              <FormField
+                  name="name"
+                  control={form.control}
+                  render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input type="text" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          Ingresa tu nombre completo
+                        </FormDescription>
+                      </FormItem>
+                  )}
+              />
+              <FormItem>
+                <FormLabel htmlFor="lastName">LastName</FormLabel>
+                <FormDescription>
+                  Ingresa tu apellido
+                </FormDescription>
+                <FormControl>
+                  <Input id="lastName" type="text" {...form.register("lastName")} />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel htmlFor="age">Age</FormLabel>
+                <FormDescription>
+                  Ingresa tu edad
+                </FormDescription>
+                <FormControl>
+                  <Input id="age" type="text" {...form.register("age")} />
+                </FormControl>
+              </FormItem>
+              <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select a verified email to display" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="m@example.com">admin@petal.com</SelectItem>
+                            <SelectItem value="m@google.com">price@petal.com</SelectItem>
+                            <SelectItem value="m@support.com">tool@price.com</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormDescription>
+                          You can manage email addresses in your{" "}
+                          <Link href="/">email settings</Link>.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                  )}
+              />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+              <Button type="submit">Guardar</Button>
+            </form>
+          </FormProvider>
+
+        </CardContent>
+
+      </Card>
   );
 }
+
+export default Page
